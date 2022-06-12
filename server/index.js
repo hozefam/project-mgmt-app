d1a38f075ec24945eb8087e55f276a7e4fa7e478
Hozefa Mangalorewala <hozefam@gmail.com>
2022-06-12 20:37:26 +0300

    Added db connection and models

--- server/index.js
+++ server/index.js
@@ -2,9 +2,14 @@ const express = require("express");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const connectDB = require("./config/db.js");
 const port = process.env.PORT || 5000;
+const colors = require("colors");
 
 const app = express();
+
+// Connect to database
+connectDB();
 
 app.use(
   "/graphql",
